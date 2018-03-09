98c504544608f0d6b9f128e61c7ed010589b18b8
YniRus <ynirus@yandex.ru>
2018-03-09 22:27:03 +0300

    Revert "Revert "Новый скил и баг-фиксы""
    This reverts commit 1a3d3ee6f9762e446bdd6ebba3ebe5109df394e8.

--- GameRPG/Hero.cs
+++ GameRPG/Hero.cs
@@ -52,16 +52,7 @@ namespace GameRPG
                         CurrentTime = 0;
                         CurrentFrame = 0;
                     }
-                }
-                //Нажата кнопка ВВВЕРХ
-                else if (kbState.IsKeyDown(Keys.Space))
-                {
-                    if (Rectangle.X < Window.ClientBounds.Width - FrameWidth)
-                    {
-                        State = 4;
-                        onJump = true;
-                    }
-                }
+                }                
                 //Ничего не нажато
                 else
                 {
@@ -72,6 +63,16 @@ namespace GameRPG
                         CurrentFrame = 0;
                     }
                 }
+                //Нажат ПРОБЕЛ
+                if (kbState.IsKeyDown(Keys.Space))
+                {
+                    if (Rectangle.X < Window.ClientBounds.Width - FrameWidth)
+                    {
+                        State = 4;
+                        onJump = true;
+                    }
+                }
+
             }
         }
     }
